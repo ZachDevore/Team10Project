300f185a00c1990f9d9dfd85932e9be09e01ef40
Chalfton <zachjdevore@hotmail.com>
2023-02-18 15:50:50 -0500

    Add Navigation Bar Component
    Created Navigation Bar as well as install sqllite and react icons

--- TheStoreApp/the_store_app/src/App.js
+++ TheStoreApp/the_store_app/src/App.js
@@ -1,25 +1,25 @@
 import logo from './logo.svg';
-import './App.css';
+import NavigationBar from './Components/NavigationBar';
+import {Route, Routes} from "react-router-dom";
+import IndividualProductView from './Pages/IndividualProductView';
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <IndividualProductView />
+    </>
   );
 }
+
+/* This is how to set up routing
+ <div className="container">
+        <Routes>
+         <Route path="/" element={<Home />} />
+         <Route path="/pricing" element={<Pricing />} />
+         <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
+      */
 
 export default App;
---
+++ TheStoreApp/the_store_app/src/Pages/IndividualProductView.js
@@ -0,0 +1,12 @@
+import NavigationBar from "../Components/NavigationBar";
+import '/in'
+export default function IndividualProductView() {
+    return (
+        <>
+        <NavigationBar />
+        <div>
+
+        </div>
+       </>
+    );
+}
