a56c9fa71a759aa4ecfe5e8692513e49990d3550
Chalfton <zachjdevore@hotmail.com>
2023-04-22 18:23:04 -0400

    add company logo, change navigation bar to a vertical layout

--- TheStoreApp/the_store_app/src/Components/NavigationBar.js
+++ TheStoreApp/the_store_app/src/Components/NavigationBar.js
@@ -2,21 +2,26 @@ import { NavLink, useMatch, useResolvedPath } from "react-router-dom";
 import {FcDvdLogo} from 'react-icons/fc';
 import {BsSearch} from 'react-icons/bs';
 import PopUp from "./PopUp";
+import "../Styles/navigationBarStyles.css"
+
 export default function NavigationBar() {
     return (
-    <nav className="nav">
-         <FcDvdLogo size={80}/>
-         <div className="search">
-         <BsSearch />
-        <input className="searchBar"></input>
-        </div>
-        <ul>
-            <PopUp />
-            <NavLink to="/SignUpPage">Sign in</NavLink>            
-            <NavLink to="/AddProduct">Add Item</NavLink>
-            <NavLink to="/AboutPage">About Us</NavLink> 
-            <NavLink to="/">Today's Deals</NavLink>
+    <>
+        <ul className="navBar">
+            
+            <li> <img src="img/companyLogo.jpg" alt="Company Logo"></img> </li>
+            <li> <PopUp /> </li>
+            {/* <NavLink className="nav" to="/SignUpPage">Sign in</NavLink>             */}
+           <li> <NavLink className="nav" to="/AddProduct">Add Item</NavLink> </li> 
+           <li> <NavLink className="nav" to="/AboutPage">About Us</NavLink> </li>
+           <li> <NavLink className="nav" to="/">Today's Deals</NavLink> </li>
+          
+           <li>   <input 
+                    className="searchBar"
+                    type="text"
+                    placeholder="Search" /> 
+            </li>
         </ul>
-    </nav>
+    </>
     );
 }
